952daf18e0f7dc49f8f1af316cd87b6d0a2fae6b
Chase Ramsey <ramsey.chase@gmail.com>
2016-07-19 13:20:14 -0500

    Wrote tests for basic domain analysis logic

---
+++ domain_tests.py
@@ -0,0 +1,33 @@
+import unittest
+import string
+from bespoken import *
+from domain import *
+
+class TestDomainAnalysis(unittest.TestCase):
+
+  @classmethod
+  def setUpClass(self):
+    self.tokenizer = Bespokenize()
+    self.domain_mod = DomainModule()
+
+    self.test_string = 'Oh man, I love sentences. Do you also love sentences?'
+    self.tokenizer.parse_phrase(self.test_string)
+
+  def test_module_creates_list_of_sentences_without_punctuation(self):
+    self.assertIsInstance(self.domain_mod.dom_parsed, list)
+    self.assertTrue(len(self.domain_mod.dom_parsed) > 0)
+    self.assertNotTrue(True in [punc in self.domain_mod.dom_parsed for punc in string.punctuation])
+
+  def test_module_creates_list_of_domain_words_in_sentences(self):
+    self.assertEqual(self.domain_mod.find_domains, [['people', 'preference', 'grammar'], ['preference', 'grammar']])
+
+  def test_module_creates_dictionary_for_report(self):
+    report = {
+      'people': 0.25,
+      'preference': 0.4583,
+      'grammar': 0.2917
+    }
+    self.assertEqual(self.domain_mod.create_report, report)
+
+if __name__ == '__main__':
+  unittest.main()
