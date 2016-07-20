12d8b1ecb0c9ef4488abc1cb57ca5ae48a806b89
Chase Ramsey <ramsey.chase@gmail.com>
2016-07-20 10:26:51 -0500

    Built out domain analysis module with 'pass' functions

---
+++ domain.py
@@ -0,0 +1,16 @@
+class DomainModule:
+
+  def __init__(self):
+    self.dom_parsed = ''
+
+  def process_tokenizer_list(self):
+    pass
+
+  def find_keywords(self):
+    pass
+
+  def find_domains(self):
+    pass
+
+  def create_report(self):
+    pass
