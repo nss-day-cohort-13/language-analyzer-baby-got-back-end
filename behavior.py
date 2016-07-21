74986c57395e9e8d5a64ca6d42baeccc5b68381e
Tekisha Hammock <tekisha.hammock@gmail.com>
2016-07-21 10:16:28 -0500

    more updates to the behavior testing

---
+++ behavior.py
@@ -0,0 +1,7 @@
+from bespoken import *
+
+class Behavior:
+  def __init__(self, phrase):
+    self.tokenize = Bespokenize()
+    self.tokenizer.parse_phrase(self.test_string)
+
--- behavior_test.py
+++ behavior_test.py
@@ -14,19 +14,23 @@ class TestBehavior(unittest.TestCase):
 
     self.test_string = "The challenge of space exploration and particularly of landing men on the moon represents the greatest challenge which has ever faced the human race. Even if there were no clear scientific or other arguments for proceeding with this task, the whole history of our civilization would still impel men toward the goal."
     self.tokenizer.parse_phrase(self.test_string)
-
-  def test_module_separate_tokenize_list_into_sentence_lists_if_multiple(self):
-    self.assertIsInstance(self.tokenizer.sentences_list[0], list)
+    self.tokenizer.sentences_list(self.tokenizer.parsed)
 
   def test_module_assign_behavior_to_keywords_in_one_sentence(self):
-    self.assertIsInstance(self.tokenizer.sentences_list[0].keywords, list)
-    self.assertTrue(self.lexicon.behaviors.inquisitive.__contains__('exploration'))
-    self.assertEqual(self.behavior.sentence_behavior_list[-1], 'inquisitive')
+    self.assertIsInstance(self.behavior.sentence_keywords, list)
+    self.assertTrue(len(self.behavior.sentence_keywords) == len(self.behavior.sentence_behaviors))
+    self.assertIsInstance(self.behavior.sentence_behavior_list, list)
+    self.assertIsInstance(self.behavior.sentence_behavior_list[0], list)
+    self.assertTrue(len(self.behavior.sentence_behavior_list[0]) > 0)
+
+  def test_module_sentiment_analysis_lists(self):
+    self.assertIsInstance(self.behavior.behavior_modifiers, list)
+    self.assertIsInstance(self.behavior.behavior_modifiers[0], list)
 
   def test_module_assign_numeric_value(self):
-    self.assertIsInstance(self.behavior.sentence_behavior_list, list)
-    self.assertIsInstance(self.behavior.behavior_number[0], dict)
-    self.assertTrue(0 < self.behavior.behavior_percentage[0] <= 1)
+    self.assertIsInstance(self.behavior.behavior_counters[0][0], dict)
+    self.assertIsInstance(self.behavior.report, dict)
+    self.assertTrue(0 < self.behavior.report.values() <= 1)
 
 if __name__ == '__main__':
   unittest.main()
