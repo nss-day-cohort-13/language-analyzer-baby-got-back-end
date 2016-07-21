e0d10c9e35bfd8ee9a46840d74639a9c4bdf1b0c
Cory Tohill <corytohill@gmail.com>
2016-07-21 13:02:25 -0500

    added doc strings to all methods

--- bespoken.py
+++ bespoken.py
@@ -2,12 +2,39 @@ import re
 import string
 
 class Bespokenize:
+    """Contains methods for tokenizing a phrase
+
+    Methods:
+    -----------------
+    parse_phrase
+    seperate_into_sentences
+    filter_punctuation
+    word_count
+    total_word_count
+    alphanum_characters
+    sentence_count
+    get_position
+    """
     def parse_phrase(self, phrase):
+        """Parse a phrase by words and punctuation and store in self.parsed
+
+        Method arguments:
+        -----------------
+        phrase(string) -- The phrase to be parsed
+        """
+
         phrase = phrase.lower()
         self.parsed = re.findall(r"[\w']+|[.,!?;]", phrase)
         return self.parsed
 
     def seperate_into_sentences(self):
+        """Lists sentences present in self.parsed
+
+        Method arguments:
+        -----------------
+        n/a
+        """
+
         end_punctuation = [".", "?", "!"]
         current_sentence = []
         sentences_list = []
@@ -22,6 +49,13 @@ class Bespokenize:
         return sentences_list
 
     def filter_punctuation(self):
+        """Lists sentences without punctuation from self.parsed
+
+        Method arguments:
+        -----------------
+        n/a
+        """
+
         sentences_list = self.seperate_into_sentences()
         punctuation = string.punctuation
         filtered_sentences_list = []
@@ -35,6 +69,13 @@ class Bespokenize:
         return filtered_sentences_list
 
     def word_count(self):
+        """List number of words in each sentence in self.parsed
+
+        Method arguments:
+        -----------------
+        n/a
+        """
+
         sentences = self.filter_punctuation()
         words_per_sentence = []
         for sentence in sentences:
@@ -43,6 +84,12 @@ class Bespokenize:
         return words_per_sentence
 
     def total_word_count(self):
+        """List number of words in the entire phrase in self.parsed
+
+        Method arguments:
+        -----------------
+        n/a
+        """
         sentence_word_count = self.word_count()
         total_words = 0
 
@@ -52,6 +99,12 @@ class Bespokenize:
         return total_words
 
     def alphanum_characters(self):
+        """Lists all alphanumeric characters used in self.parsed
+
+        Method arguments:
+        -----------------
+        n/a
+        """
         alphanums_used = set()
 
         for item in self.parsed:
@@ -63,10 +116,24 @@ class Bespokenize:
         return ''.join(char for char in alphanums_used)
 
     def sentence_count(self):
+        """Lists number of sentences present in self.parsed
+
+        Method arguments:
+        -----------------
+        n/a
+        """
+
         sentences = self.seperate_into_sentences()
         return len(sentences)
 
     def get_position(self, word):
+        """Lists the position of a word based on all words in self.parsed
+
+        Method arguments:
+        -----------------
+        word(string) -- The word to get the position of
+        """
+
         sentences = self.filter_punctuation()
         filtered_phrase = []
 
