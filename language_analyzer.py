d5486e9c91130d99f9d482ed69285322bcf7fb96
Chase Ramsey <ramsey.chase@gmail.com>
2016-07-22 09:36:22 -0500

    Added docstrings to language_analyzer.py

--- language_analyzer.py
+++ language_analyzer.py
@@ -6,12 +6,25 @@ from domain import *
 class LanguageAnalyzer:
 
   def __init__(self, phrase):
+    '''When instantiated, this module instantiates all of the
+       discrete language analysis modules
 
+       Arguments:
+       ----------
+       phrase - the phrase to be analyzed; passed down for processing
+                in each discrete analysis module
+    '''
     # self.sentiment_module = Sentiment(phrase)
     # self.behavior_module = Behavior(phrase)
     self.domain_module = Domain(phrase)
 
   def lang_run_all(self):
+    '''Runs all of the 'run_all' functions in each module
+
+    Method arguments:
+    -----------------
+    n/as
+    '''
     # self.sentiment_module.run_all_sentiment()
     # self.behavior_module.run_all_behavior()
     self.domain_module.run_all_domain()
