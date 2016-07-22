e3d10c46ff235d43e42f6348340b6e49f0b08e37
Tekisha Hammock <tekisha.hammock@gmail.com>
2016-07-22 09:59:36 -0500

    FINISHED BEHAVIOR MODULE

--- behavior.py
+++ behavior.py
@@ -1,7 +1,77 @@
 from bespoken import *
+from lexicon import *
+from collections import Counter
 
 class Behavior:
   def __init__(self, phrase):
-    self.tokenize = Bespokenize()
-    self.tokenizer.parse_phrase(self.test_string)
+    self.tokenizer = Bespokenize()
+    self.tokenizer.parse_phrase(phrase)
+    self.sentences = self.tokenizer.separate_into_sentences()
+    self.sentence_behavior_list = list()
+    self.behavior_percentage_weighted = list()
+
+  def find_behaviors(self):
+    for sentence in self.sentences:
+      self.sentence_behaviors = list()
+      self.sentence_behavior_list.append(self.sentence_behaviors)
+      for word in sentence:
+        for behavior, keys in lexicon[0]["behavior"].items():
+          for keyword in keys:
+            if word == keyword:
+              self.sentence_behaviors.append(behavior)
+
+    print(self.sentence_behavior_list)
+    return self.sentence_behavior_list
+
+  def find_weighted_behavior_value(self):
+    self.behavior_count = list()
+    for self.sentence_behaviors in self.sentence_behavior_list:
+      self.behavior_instance_count = {behavior:self.sentence_behaviors.count(behavior) for behavior in self.sentence_behaviors}
+      self.behavior_count.append(self.behavior_instance_count)
+
+    for self.behavior_instance_count in self.behavior_count:
+      self.behavior_percent = dict()
+      self.behavior_percentage_weighted.append(self.behavior_percent)
+      for behavior, value in self.behavior_instance_count.items():
+        raw_percent = value / sum(self.behavior_instance_count.values())
+        percent = float("{0:.2f}".format(raw_percent))
+        self.behavior_percent[behavior] = percent
+
+  def generate_phrase_behavior_report(self):
+    self.sentence_count_sum = 0
+    for self.behavior_percent in self.behavior_percentage_weighted:
+      self.sentence_count_sum += sum(self.behavior_percent.values())
+
+    self.phrase_averaged_behaviors = list()
+    for self.behavior_percent in self.behavior_percentage_weighted:
+      self.sentence_averaged_behaviors = dict()
+      self.phrase_averaged_behaviors.append(self.sentence_averaged_behaviors)
+      for behavior, value in self.behavior_percent.items():
+        raw_weighted_value = value / self.sentence_count_sum
+        rounded_value = float("{0:.2f}".format(raw_weighted_value))
+        self.sentence_averaged_behaviors[behavior] = rounded_value
+
+    self.raw_report = sum((Counter(self.sentence_averaged_behaviors) for self.sentence_averaged_behaviors in self.phrase_averaged_behaviors),Counter())
+
+    self.report = dict()
+    for behavior, value in self.raw_report.items():
+      rounded_final = float("{0:.2f}".format(value))
+      self.report[behavior] = rounded_final
+
+    print(self.report)
+    return self.report
+
+  def print_report(self):
+    print(' ')
+    print('---------- DOMAIN ANALYSIS RESULTS ----------')
+    print(' ')
+    for key, value in self.report.items():
+      print('  {0}: {1}'.format(key.capitalize(), value))
+    print(' ')
+
+  def run_all_behavior(self):
+    self.find_behaviors()
+    self.find_weighted_behavior_value()
+    self.generate_phrase_behavior_report()
+    self.print_report()
 
--- behavior_test.py
+++ behavior_test.py
@@ -8,29 +8,33 @@ class TestBehavior(unittest.TestCase):
   @classmethod
   def setUpClass(self):
 
-    self.behavior = Behavior()
-    self.tokenizer = Bespokenize()
-    self.lexicon = Lexicon()
-
     self.test_string = "The challenge of space exploration and particularly of landing men on the moon represents the greatest challenge which has ever faced the human race. Even if there were no clear scientific or other arguments for proceeding with this task, the whole history of our civilization would still impel men toward the goal."
-    self.tokenizer.parse_phrase(self.test_string)
-    self.tokenizer.sentences_list(self.tokenizer.parsed)
+    self.behavior = Behavior(self.test_string)
+    self.tokenizer = Bespokenize()
+    self.lexicon = lexicon
+    self.behavior.find_behaviors()
+    self.behavior.find_weighted_behavior_value()
+    self.behavior.generate_phrase_behavior_report()
+    self.behavior.print_report()
 
-  def test_module_assign_behavior_to_keywords_in_one_sentence(self):
-    self.assertIsInstance(self.behavior.sentence_keywords, list)
-    self.assertTrue(len(self.behavior.sentence_keywords) == len(self.behavior.sentence_behaviors))
+  # Done
+  def test_module_iterate_parsed_sentence_for_behaviors(self):
+    self.assertIsInstance(self.behavior.sentence_behaviors, list)
     self.assertIsInstance(self.behavior.sentence_behavior_list, list)
     self.assertIsInstance(self.behavior.sentence_behavior_list[0], list)
     self.assertTrue(len(self.behavior.sentence_behavior_list[0]) > 0)
 
-  def test_module_sentiment_analysis_lists(self):
-    self.assertIsInstance(self.behavior.behavior_modifiers, list)
-    self.assertIsInstance(self.behavior.behavior_modifiers[0], list)
+  # Done
+  def test_module_get_percentage_by_sentence(self):
+    self.assertIsInstance(self.behavior.behavior_count, list)
+    self.assertIsInstance(self.behavior.behavior_percentage_weighted, list)
 
-  def test_module_assign_numeric_value(self):
-    self.assertIsInstance(self.behavior.behavior_counters[0][0], dict)
+  # Done
+  def test_module_generating_report_logic(self):
+    self.assertIsInstance(self.behavior.sentence_count_sum, float)
+    self.assertIsInstance(self.behavior.phrase_averaged_behaviors, list)
+    self.assertIsInstance(self.behavior.raw_report, dict)
     self.assertIsInstance(self.behavior.report, dict)
-    self.assertTrue(0 < self.behavior.report.values() <= 1)
 
 if __name__ == '__main__':
   unittest.main()
